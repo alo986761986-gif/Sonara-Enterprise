400a8c0b3f3e3a316f16f9a13e97b5152345c9a7
ALOGITSONARA <alo986761986@gmail.com>
2026-08-07 20:57:22 +0200

    checkpoint working auth gate and guest login

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import {
+  SonaraAuthGate,
+  SonaraAuthProvider
+} from './auth/SonaraAuth';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <SonaraAuthProvider>
+      <SonaraAuthGate>
+        <App />
+      </SonaraAuthGate>
+    </SonaraAuthProvider>
   </React.StrictMode>
 );
