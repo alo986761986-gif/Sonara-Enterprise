986dbac7d74d5c1dfba440d15ee86f63d825c6ed
alo986761986-gif <alo986761986@gmail.com>
2026-08-19 16:54:19 +0200

    Remove LeVo engine export

--- backend/src/engine/index.ts
+++ backend/src/engine/index.ts
@@ -1,5 +1,4 @@
 export * from './IAudioGenerationEngine';
 export * from './AceStepEngine';
-export * from './LevoEngine';
 export * from './EngineDiagnosticService';
 export * from './PythonEnvironmentManager';
