2dca32dca21e57d5d9e026da84433f8de509a0b0
alo986761986-gif <alo986761986@gmail.com>
2026-08-24 01:33:52 +0200

    Activate prompt-only Random behavior

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import BootAuth from './components/auth/BootAuth';
+import { installRandomPromptGuard } from './randomPromptGuard';
 import './index.css';
+
+installRandomPromptGuard();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
