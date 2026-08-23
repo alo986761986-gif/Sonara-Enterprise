f3c681d9295b8aeb4b566da8ae0d68fa34aaacd3
alo986761986-gif <alo986761986@gmail.com>
2026-08-24 01:52:44 +0200

    Remove obsolete browser style-lock layer

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import BootAuth from './components/auth/BootAuth';
 import { installRandomPromptGuard } from './randomPromptGuard';
-import { installGenerationStyleGuard } from './generationStyleGuard';
 import './index.css';
 
 installRandomPromptGuard();
-installGenerationStyleGuard();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
