3f823f095c7de7c1a023c891228ddcab5ffa6004
alo986761986-gif <alo986761986@gmail.com>
2026-08-23 23:30:12 +0200

    Enable Sonara boot and authentication portal

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import BootAuth from './components/auth/BootAuth';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BootAuth>
+      <App />
+    </BootAuth>
   </React.StrictMode>
 );
