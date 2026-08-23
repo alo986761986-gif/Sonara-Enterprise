3945952217c87b0fba67f29201f4c440b908b65d
alo986761986-gif <alo986761986@gmail.com>
2026-08-24 01:45:23 +0200

    Enforce selected Sonara genre hierarchy before generation

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import BootAuth from './components/auth/BootAuth';
 import { installRandomPromptGuard } from './randomPromptGuard';
+import { installGenerationStyleGuard } from './generationStyleGuard';
 import './index.css';
 
 installRandomPromptGuard();
+installGenerationStyleGuard();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
