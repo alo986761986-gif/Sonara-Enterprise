565715a50cc378faeb78565c3abdea9fa97bf401
alo986761986-gif <alo986761986@gmail.com>
2026-08-27 16:37:08 +0200

    feat(generator): mount Sonara creator skin

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import IntelligentPromptControl from './components/generator/IntelligentPromptCo
 import IntelligentLyricsControl from './components/generator/IntelligentLyricsControl';
 import VocalCharacterControl from './components/generator/VocalCharacterControl';
 import DualTrackGenerationControl from './components/generator/DualTrackGenerationControl';
+import SonaraCreatorSkin from './components/generator/SonaraCreatorSkin';
 import StudioSectionControl from './components/studio/StudioSectionControl';
 import DJSectionControl from './components/dj/DJSectionControl';
 import './index.css';
@@ -25,6 +26,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <IntelligentLyricsControl />
         <VocalCharacterControl />
         <DualTrackGenerationControl />
+        <SonaraCreatorSkin />
         <StudioSectionControl />
         <DJSectionControl />
       </>
